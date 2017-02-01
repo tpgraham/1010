e222297c0f60efd7ca02b9b90ea81850740de3f7
Grace Glenn <mgglenn@g.clemson.edu>
2017-02-01 15:08:53 -0500

    small patch

---
+++ grading/lab01/submissions/bucknerclayc_17602_1055309_lab1.c
@@ -0,0 +1,17 @@
+/*
+Clay Buckner
+CpSc 1010 Lab, Spring 2017
+Lab #1
+My first “hello world” program.
+*/
+#include <stdio.h>
+int main(void) {
+	printf("Hello world\n");
+	printf("Hello world\n");
+	printf("Hello world\n");
+	printf("Hello world\n");
+	printf("Hello world\n");
+	printf("Hello world\nHello world\nHello world\nHello world\nHello world\n");
+
+	return(0);
+}
---
+++ grading/lab01/submissions/smithcoltonh_33788_1068606_lab1-1.c
@@ -0,0 +1,25 @@
+/*	Colton Smith
+	CpSc 1010 Lab, Spring 2017
+	Lab #1
+	Section #2
+	This program is a modified version of simple.c that displays "Hello World" ten times.
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+	printf ("Hello world\n");
+
+	return(0);
+
+}
